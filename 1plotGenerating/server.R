f89688478b5af8d997a5d5702894362ea9fbfc66
speedyjwc <speedy_jwc@hotmail.com>
2024-05-22 17:42:32 +1200

    updated the accordion title style and behaviour(partly done)

--- 1plotGenerating/server.R
+++ 1plotGenerating/server.R
@@ -11,6 +11,7 @@ shinyServer(function(input, output, session) {
     accordion_panel_close(id = "sideAcc", values = TRUE)
   })
   
+  # raw data frame after the csv is uploaded
   df_raw <- reactive({
     req(input$file1)
     tryCatch(
@@ -35,7 +36,90 @@ shinyServer(function(input, output, session) {
     # df_raw(), 
     datatable(df_raw(), options = list(scrollX=TRUE) )
     # ,selection = list(mode = 'single', target = 'column'))
-    
   )
+  
+  # update the dropdown input for varaible selection
+  observe ({
+    df_num <- df_raw() %>%
+      # only keep the numeric variable (potential issue?)
+      select_if(~is.numeric(.x))
+    # choices <- c("Choose_Here",names(df_num))
+    updatePickerInput(session, inputId = "num1", choices = sort(names(df_num)))
+    
+  })
+  
+  # store the x-axis label value when update button is clicked
+  xLabBox <- eventReactive(input$updatePlotButton, {
+    input$xLabBox
+  })
+  
+  observe({
+    updateTextInput(session, inputId = "xLabBox", value = input$num1)
+  })
+  
+  observe({
+    updateTextInput(session, inputId = "generatePlotButton", value = input$num1)
+    
+  })
+  
+  # store the y-axis label value when update button is clicked
+  yLabBox <- eventReactive(input$updatePlotButton, {
+    input$yLabBox
+  })
+  
+  # store the title value when update button is clicked
+  titleBox <- eventReactive(input$updatePlotButton, {
+    input$titleBox
+  })
+  
+  boxplot <- reactive({
+    df = df_raw()[c(input$num1)]
+    
+    # p <- ggplot(df_sub(), aes_string(y = names(df_sub())[1], x = 0)) + 
+    p <- ggplot(df, aes_string(y = input$num1, x = 0)) + 
+      geom_boxplot() +
+      coord_flip()
+    
+    
+    fig <- plotly_build(p)
+    
+    fig$x$data[[1]]$fillcolor <- input$colBox
+    
+    fig$x$data[[1]]$marker$opacity <- 0.5
+    
+    boxOutput <- fig %>% 
+      hide_colorbar() %>% 
+      add_trace(x = mean(df[,1],na.rm = TRUE), y=0, 
+                type = "scatter", mode = "marker",
+                marker = list(symbol = "x",
+                              size = 10,
+                              color = "deepskyblue"),
+                hoverinfo = "text",
+                text = ~paste("Mean: ",round(mean(df[,1],na.rm = TRUE),2), sep="")) %>% 
+      layout(plot_bgcolor='#e5ecf6',
+             title = titleBox(),
+             xaxis = list(title = xLabBox(),
+                          gridcolor = 'ffff'
+             ),
+             yaxis = list(gridcolor = 'ffff',
+                          showgrid = F,
+                          title = yLabBox(),
+                          ticklen = 0,
+                          showticklabels=FALSE
+             ) 
+      )%>%
+      config(modeBarButtons = list(list("toImage")),
+             displaylogo = FALSE)
+    
+    return(boxOutput)
+  })
+  
+  output$boxPlot <- renderPlotly({
+    boxplot()
+  })
+  
+  
+  
+  
 })
 
--- 1plotGenerating/ui.R
+++ 1plotGenerating/ui.R
@@ -2,216 +2,284 @@ library(shiny)
 library(bslib)
 library(bsicons)
 library(shinyWidgets)
+library(DT)
+library(colourpicker)
 
 # Define UI 
 shinyUI(
-  page_fillable(
-    padding = 0,
-    theme = bs_theme(version = 5, bootswatch = "united"),
+  tags$head(
     
-    # sidebar for all types/numbers of variable
-    layout_sidebar(
+    page_fillable(
+      padding = 0,
+     
+      theme = bs_theme(version = 5,
+                       bootswatch = "united",
+      #                  # "accordion-title-color" = "green",
+      #                  heading_font = font_google("Josefin Slab")
+      ),
       
-      # Sidebar panel for inputs ----
-      sidebar = sidebar(
-
-        # Use accordion layout
-        accordion(
-          id = "sideAcc",
-          layout_columns(
-            col_widths = c(6, 6),
-            actionBttn(
-              inputId = "isExpand",
-              label = "Expand all",
-              style = "float",
-              color = "primary",
-              size = "sm"
-            ),
-            actionBttn(
-              inputId = "isCollapse",
-              label = "Collapse all",
-              style = "float",
-              color = "primary",
-              size = "sm"
-            )
-          ),
-
-
-          # accordion 1, upload file
-          accordion_panel( 
-            # suppose to use "value" to identify each panel
-            value = "acc_file",
-            "Uploading Files",
-            icon = bs_icon("upload"),
-            # Input: Select a file ----
-            fileInput(
-              "file1",
-              "Choose CSV File",
-              multiple = TRUE,
-              accept = c(
-                "text/csv",
-                "text/comma-separated-values,text/plain",
-                ".csv"
+      # ?alternative way to customise the accordion titles
+      tags$style(HTML(
+        ".accordion-title{
+                          /*customise the title font style(bold, font family and italic)*/
+                          /*color: green;*/
+                          font-weight: 1000;
+                          font-style: italic;
+                          font-family: 'Josefin Slab', 'Courier New', monospace;
+                          /*add a green underline*/
+                          text-decoration-line: underline;
+                          text-decoration-color: green;  
+                                 }
+               ")),
+     
+      
+      # sidebar for all types/numbers of variable
+      layout_sidebar(
+        
+        # Sidebar panel for inputs ----
+        sidebar = sidebar(
+          
+          
+          # Use accordion layout
+          accordion(
+            id = "sideAcc",
+            layout_columns(
+              col_widths = c(6, 6),
+              actionBttn(
+                inputId = "isExpand",
+                label = "Expand all",
+                style = "float",
+                color = "primary",
+                size = "sm"
+              ),
+              actionBttn(
+                inputId = "isCollapse",
+                label = "Collapse all",
+                style = "float",
+                color = "primary",
+                size = "sm"
               )
             ),
- 
             
-            # # Horizontal line ----
-            # tags$hr(),
             
-            # Input: Checkbox if file has header ----
-            checkboxInput("header", "Header", TRUE),
-            
-            # Input: Select separator ----
-            radioButtons(
-              "sep",
-              "Separator",
-              choices = c(
-                Comma = ",",
-                Semicolon = ";",
-                Tab = "\t"
+            # accordion 1, upload file
+            accordion_panel( 
+              # suppose to use "value" to identify each panel
+              value = "acc_file",
+              "Uploading Files",
+              icon = bs_icon("upload"),
+              # Input: Select a file ----
+              fileInput(
+                "file1",
+                "Choose CSV File",
+                multiple = TRUE,
+                accept = c(
+                  "text/csv",
+                  "text/comma-separated-values,text/plain",
+                  ".csv"
+                )
               ),
-              selected = ","
+              
+              
+              # # Horizontal line ----
+              # tags$hr(),
+              
+              # Input: Checkbox if file has header ----
+              checkboxInput("header", "Header", TRUE),
+              
+              # Input: Select separator ----
+              radioButtons(
+                "sep",
+                "Separator",
+                choices = c(
+                  Comma = ",",
+                  Semicolon = ";",
+                  Tab = "\t"
+                ),
+                selected = ","
+              ),
+              
+              # Input: Select quotes ----
+              radioButtons(
+                "quote",
+                "Quote",
+                choices = c(
+                  None = "",
+                  "Double Quote" = '"',
+                  "Single Quote" = "'"
+                ),
+                selected = '"'
+              ),
+              
+              # # Horizontal line ----
+              # tags$hr(),
+              # 
+              # # Input: Select number of rows to display ----
+              # radioButtons(
+              #   "disp",
+              #   "Display",
+              #   choices = c(
+              #     Head = "head",
+              #     All = "all"
+              #   ),
+              #   selected = "head"
+              # )
             ),
             
-            # Input: Select quotes ----
-            radioButtons(
-              "quote",
-              "Quote",
-              choices = c(
-                None = "",
-                "Double Quote" = '"',
-                "Single Quote" = "'"
+            # accordion 2, variable selection
+            accordion_panel(
+              value = "acc_var",
+              icon = bs_icon("hand-index-thumb"),
+              "Variable selection",
+              open = FALSE,
+              pickerInput(
+                inputId = "num1",
+                label = "Choose a numerical variable", 
+                choices = "Upload the csv file first",
+                options = list(
+                  `live-search` = TRUE,
+                  size = 5)
               ),
-              selected = '"'
+              # actionButton("generatePlotButton", "Generate Plot", icon("refresh")),
+              actionBttn(
+                inputId = "generatePlotButton",
+                label = "Generate Plot",
+                style = "unite", 
+                # color = "danger"
+              )
             ),
             
-            # # Horizontal line ----
-            # tags$hr(),
-            # 
-            # # Input: Select number of rows to display ----
-            # radioButtons(
-            #   "disp",
-            #   "Display",
-            #   choices = c(
-            #     Head = "head",
-            #     All = "all"
-            #   ),
-            #   selected = "head"
-            # )
-          ),
-
-          # accordion 2, variable selection
-          accordion_panel(
-            value = "acc_var",
-            icon = bs_icon("hand-index-thumb"),
-            "Variable selection",
-            open = FALSE,
-            "Choose the variable for visualisiation"
-          ),
-          
-          # accordion 3, plot customise
-          accordion_panel(
-            value = "acc_plot",
-            icon = bs_icon("palette"),
-            "Plot editting",
-            open = FALSE,
-            "Customise the plot"
+            # accordion 3, plot customise
+            conditionalPanel(
+              condition = "input.generatePlotButton > 0 & input.num1 != 'Upload the csv file first'",
+              
+              accordion_panel(
+                value = "acc_plot",
+                icon = bs_icon("palette"),
+                "Plot editting",
+                open = FALSE,
+                "Customise the plot",
+                actionButton("updatePlotButton", "Update Plot", icon("refresh")),
+                colourInput(inputId = "colBox", 
+                            label = "Select colour to fill the box", 
+                            value = "salmon"),
+                hr(),
+                h4("Customised Labeling:"),
+                textInput("titleBox", label = "Title:  ", " "),
+                # x axis
+                textInput(inputId = "xLabBox", 
+                          label = "X Label:", 
+                          value = "X axis is..."),
+                # y axis
+                textInput("yLabBox", "Y Label:", " ")
+              )
+            )
           )
-        )
-      ),
-
-      # main content area
-      navset_pill(
-        # title for each panel was too long for tab/pill, generate a menu to host them
-        nav_menu(
-          title = "Select the number of variable and the type",
-          nav_panel(title = "One Quantitative Variable", 
-                    icon = bs_icon("1-circle-fill"),
-                    p("First tab content."),
-                    verbatimTextOutput("test"),
-                    
-                    navset_card_pill(
-                      placement = "above",
-                      nav_panel(
-                        title = "Raw data",
-                        h4("upload a csv file and select a numerical variable"),
-                        br(),
-                        # Output: Data file ----
-                        dataTableOutput("contents_raw")),
-                      nav_panel(
-                        
-                        title = "Vis1",
-                        p("plot 1 content.")
-                      ),
-                      nav_panel(title = "Vis2", 
-                                p("plot 2 content.")),
-                      nav_panel(title = "Vis3", 
-                                p("plot 3 content.")),
-                      
-                      nav_panel(title = "Summary", 
-                                # combined in datatable tab?
-                                p("Summary statistic content")),
-                      
-                    )
-          ),
-          nav_panel(title = "One Categorical Variable", 
-                    icon = bs_icon("2-circle-fill"),
-                    p("Second tab content."),
-                    # navset_card_pill(
-                    #   placement = "below",
-                    #   nav_panel(title = "One",
-                    #             p("First tab content."),
-                    #             #             # Output: Data file ----
-                    #             tableOutput("contents")),
-                    #   nav_panel(title = "Two", 
-                    #             p("Second tab content.")),
-                    #   nav_panel(title = "Three", 
-                    #             p("Third tab content")),
-                    #   
-                    # )
-                    
-                    
-                    
-          ),
-          nav_panel(title = "One Quantitative and One Categorical Variable",
-                    icon = bs_icon("3-circle-fill"),
-                    p("One Quantitative and One Categorical Variable content")
-          ),
-          nav_panel(title = "Two Categorical Variables", 
-                    icon = bs_icon("4-circle-fill"),
-                    p("Two Categorical Variables content")
-          ),
-          nav_panel(title = "Two Quantitative Variables", 
-                    icon = bs_icon("5-circle-fill"),
-                    p("Two Quantitative Variables content")),
         ),
-        nav_spacer(),
-        nav_item(img(src="UoC.png", style="aspect-ratio: auto; max-height: 60px")),
-        nav_menu(
-          title = "Other sites",
-          nav_item("    Sampling"),
-          nav_item("    Bootstrap"),
-          nav_item("    Randomisation"),
-          nav_item("    ...")
-        )
         
-      ),
-      
-      
-      # nav_panel(title = "Two", p("Second tab content.")),
-      # 
-      # nav_panel(title = "Three", p("Third tab content")),
-      # 
-      # nav_spacer(),
-      # nav_menu(
-      #   title = "Links",
-      #   # nav_item(link_shiny),
-      #   # nav_item(link_posit)
-      # )
-      # )
-      # )
+        # main content area
+        navset_pill(
+          
+          # title for each panel was too long for tab/pill, generate a menu to host them
+          nav_menu(
+            title = "Select the number of variable and the type",
+            nav_panel(title = "One Quantitative Variable", 
+                      icon = bs_icon("1-circle-fill"),
+                      
+                      p("One Quantitative variable."),
+                      verbatimTextOutput("test"),
+                      
+                      navset_card_pill(
+                        placement = "above",
+                        full_screen = TRUE,
+                        nav_panel(
+                          title = "Raw data",
+                          h4("upload a csv file and select a numerical variable"),
+                          br(),
+                          # Output: Data file ----
+                          DT::DTOutput("contents_raw")
+                        ),
+                        nav_panel(
+                          
+                          title = "Vis1",
+                          p("plot 1 content."),
+                          br(),
+                          plotlyOutput("boxPlot")
+                        ),
+                        nav_panel(title = "Vis2", 
+                                  p("plot 2 content.")),
+                        nav_panel(title = "Vis3", 
+                                  p("plot 3 content.")),
+                        
+                        nav_panel(title = "Summary", 
+                                  # combined in datatable tab?
+                                  p("Summary statistic content")),
+                        nav_spacer(),
+                        nav_item("One Quantitative Variable"),
+                        
+                        
+                        
+                      )
+            ),
+            nav_panel(title = "One Categorical Variable", 
+                      icon = bs_icon("2-circle-fill"),
+                      p("Second tab content."),
+                      # navset_card_pill(
+                      #   placement = "below",
+                      #   nav_panel(title = "One",
+                      #             p("First tab content."),
+                      #             #             # Output: Data file ----
+                      #             tableOutput("contents")),
+                      #   nav_panel(title = "Two", 
+                      #             p("Second tab content.")),
+                      #   nav_panel(title = "Three", 
+                      #             p("Third tab content")),
+                      #   
+                      # )
+                      
+                      
+                      
+            ),
+            nav_panel(title = "One Quantitative and One Categorical Variable",
+                      icon = bs_icon("3-circle-fill"),
+                      p("One Quantitative and One Categorical Variable content")
+            ),
+            nav_panel(title = "Two Categorical Variables", 
+                      icon = bs_icon("4-circle-fill"),
+                      p("Two Categorical Variables content")
+            ),
+            nav_panel(title = "Two Quantitative Variables", 
+                      icon = bs_icon("5-circle-fill"),
+                      p("Two Quantitative Variables content")),
+          ),
+          nav_spacer(),
+          nav_item(img(src="UoC.png", style="aspect-ratio: auto; max-height: 60px")),
+          nav_menu(
+            title = "Other sites",
+            nav_item("    Sampling"),
+            nav_item("    Bootstrap"),
+            nav_item("    Randomisation"),
+            nav_item("    ...")
+          )
+          
+        ),
+        
+        
+        # nav_panel(title = "Two", p("Second tab content.")),
+        # 
+        # nav_panel(title = "Three", p("Third tab content")),
+        # 
+        # nav_spacer(),
+        # nav_menu(
+        #   title = "Links",
+        #   # nav_item(link_shiny),
+        #   # nav_item(link_posit)
+        # )
+        # )
+        # )
+      )
     )
+    
   )
-  
 )
+  
